f108683b34f61d241e8186917a94d3d288c92ec1
Chomo <the.gamut.of.human.feelings@gmail.com>
2019-09-13 23:01:13 +0900

    EffectPoolの実装 SlashEffectの追加

---
+++ Assets/Scripts/Game/EffectPool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UniRx.Toolkit;
+
+public class EffectPool : ObjectPool<SlashEffect>
+{
+    private readonly SlashEffect _slashEffect;
+
+    private readonly Transform _transform;
+
+    public EffectPool(Transform transform, SlashEffect prefab)
+    {
+        _slashEffect = prefab;
+        _transform = transform;
+    }
+
+    //追加で生成されるときに実行
+    protected override SlashEffect CreateInstance()
+    {
+        var obj = GameObject.Instantiate(_slashEffect);
+
+        //ヒエラルキーが散らからないように一箇所にまとめる
+        obj.transform.SetParent(_transform);
+
+        return obj;
+    }
+}
---
+++ Assets/Scripts/Game/SlashEffect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+public class SlashEffect : MonoBehaviour
+{
+    [SerializeField]private double finishTime = 2.0f;
+
+    private ParticleSystem _slashEffect;
+
+    private ParticleSystem Effect
+    {
+        get
+        {
+            return _slashEffect ?? (_slashEffect = GetComponent<ParticleSystem>());
+        }
+    }
+
+
+    public IObservable<Unit> PlayEffect(Vector3 position)
+    {
+        transform.position = position;
+
+        Effect.Play();
+
+        return Observable.Timer(TimeSpan.FromSeconds(finishTime))
+             .ForEachAsync(_ => Effect.Stop());
+    }
+}
