a0ecf932bcad917d2754f5fff7e822e719100392
Chomo <the.gamut.of.human.feelings@gmail.com>
2019-09-25 12:15:50 +0900

    Enemyの行動パターンの設定できるようにした

---
+++ Assets/Scripts/Game/PlayerManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerManager : SingletonMonoBehaviour<PlayerManager>
+{
+    public GameObject player;
+
+    public Transform playerTransform { get; private set; }
+
+    private void Awake()
+    {
+        playerTransform = player.transform;
+    }
+}
