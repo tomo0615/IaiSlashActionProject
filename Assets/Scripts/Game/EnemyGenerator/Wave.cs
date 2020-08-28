1d1dbf33d226ff6bb89e06212fe81807809514a0
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-08-28 23:33:08 +0900

    EnemyGeneratorファイルの名前変更

---
+++ Assets/Scripts/Game/EnemyGenerator/Wave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Game.Enemy;
+using UnityEngine;
+
+//コンフリクトしないように名前をTestにしてます
+namespace Game.EnemyGenerator
+{
+    public class Wave : MonoBehaviour
+    {
+        private  List<BaseEnemy> enemyList = new List<BaseEnemy>();
+
+        public void InitializeWave()
+        {
+            //子のEnemyをListに格納
+            enemyList = GetComponentsInChildren<BaseEnemy>().ToList();
+        }
+
+        public bool IsKillAllEnemy()
+        {
+            return GetActiveEnemyValue() == 0;
+        }
+
+        private int GetActiveEnemyValue()
+        {
+            return enemyList.Count(enemy => enemy.gameObject.activeSelf);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/EnemyGenerator/Waves.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Game.EnemyGenerator
+{
+    public class Waves : MonoBehaviour
+    {
+        [SerializeField]
+        private float waveInterval = 1.5f;
+
+        private List<Wave> waveList = new List<Wave>();
+
+        private int currentWaveIndex = 0;
+        public Wave CurrentWave => waveList[currentWaveIndex];
+
+        public Wave LastWave => waveList[WaveMaxCount];
+
+        public int WaveMaxCount => waveList.Count;
+
+        public bool HasNextWave => currentWaveIndex + 1 < WaveMaxCount;
+
+        private void Awake()
+        {
+            InitializeWaveList();
+        }
+
+        void Start()
+        {
+            OnStartWave();
+
+            //Wave更新用
+            this.UpdateAsObservable()
+                .Where(_ => CurrentWave.IsKillAllEnemy() && HasNextWave)
+                .Subscribe(_ => StartCoroutine(OnUpdateNextWave()));
+        }
+
+        private void InitializeWaveList()
+        {
+            waveList = GetComponentsInChildren<Wave>().Select(
+                (wave, index) =>
+                {
+                    wave.InitializeWave();
+                    wave.gameObject.SetActive(false);
+                    return wave;
+                }).ToList();
+        }
+
+        public void OnStartWave()
+        {
+            CurrentWave.gameObject.SetActive(true);
+        }
+
+        private IEnumerator OnUpdateNextWave()
+        {
+            //Waveの変更(ex.1->2)
+            CurrentWave.gameObject.SetActive(false);
+
+            currentWaveIndex++;
+
+            yield return new WaitForSeconds(waveInterval);
+
+            CurrentWave.gameObject.SetActive(true);
+        }
+    }
+}
