8d451a5b648b728f0a2c5d279da78052f24ad98b
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-09-03 22:43:57 +0900

    無駄な部分を削除

--- Assets/Scripts/Game/GameStates/GameStateManager.cs
+++ Assets/Scripts/Game/GameStates/GameStateManager.cs
@@ -22,21 +22,21 @@ namespace Game.GameStates
         {
             //Setting
             {
-                var state = new State<global::Game.GameStates.GameState>(GameState.Setting);
+                var state = new State<GameState>(GameState.Setting);
                 state.SetUpAction = OnSetUpSetting;
                 state.UpdateAction = OnUpdateSetting;
                 AddState(state);
             }
             //Game
             {
-                var state = new State<global::Game.GameStates.GameState>(GameState.Game);
+                var state = new State<GameState>(GameState.Game);
                 state.SetUpAction = OnSetUpGame;
                 state.UpdateAction = OnUpdateGame;
                 AddState(state);
             }
             //Finish
             {
-                var state = new State<global::Game.GameStates.GameState>(GameState.Finish);
+                var state = new State<GameState>(GameState.Finish);
                 state.SetUpAction = OnSetUpFinish;
                 state.UpdateAction = OnUpdateFinish;
                 AddState(state);
