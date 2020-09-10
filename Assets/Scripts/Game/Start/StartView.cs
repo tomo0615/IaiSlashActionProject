3ac98f9a5dec6c664ff820cf7cddc4f31166db38
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-09-10 23:49:40 +0900

    StartViewを簡易実装

---
+++ Assets/Scripts/Game/Start/StartView.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game.Start
+{
+    public class StartView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI startText;
+        
+        public void ViewStartSign()
+        {
+            startText.enabled = true;
+        }
+    }
+}
