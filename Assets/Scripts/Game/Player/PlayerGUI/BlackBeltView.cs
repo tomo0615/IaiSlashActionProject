449204ae5126f5aa9917cea06e8d3ac023dcad10
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-09-10 23:49:21 +0900

    blackBelt Viewを実装

---
+++ Assets/Scripts/Game/Player/PlayerGUI/BlackBeltView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Game.Player.PlayerGUI
+{
+    public class BlackBeltView : MonoBehaviour
+    {
+        [SerializeField] private Image blackBeltUp;
+
+        [SerializeField] private Image blackBeltDawn;
+
+        private readonly Vector3 fadeScale = new Vector3(1, 0, 1);
+
+        public void ViewBlackBelt()
+        {
+            blackBeltUp.transform.DOScale(Vector3.one, 1.0f);
+            blackBeltDawn.transform.DOScale(Vector3.one, 1.0f);
+        }
+
+        public void FadeBlackBelt()
+        {
+            blackBeltUp.transform.DOScale(fadeScale, 1.0f);
+            blackBeltDawn.transform.DOScale(fadeScale, 1.0f);
+        }
+    }
+}
