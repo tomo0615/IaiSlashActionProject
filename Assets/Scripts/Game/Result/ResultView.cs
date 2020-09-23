5a8b319b2b4d8b4334685c51ae9d3052dee4fbd6
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-09-23 23:13:33 +0900

    ResultViewを実装中

--- Assets/Scripts/Game/Result/ResultView.cs
+++ Assets/Scripts/Game/Result/ResultView.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ResultView : MonoBehaviour
+namespace Game.Result
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ResultView : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
     }
 }
